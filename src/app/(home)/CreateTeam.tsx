b2c1d770bb8714fadf085fb5e89e7babbd191f16
unknown <jahirulislam41185@gmail.com>
2023-11-19 21:36:25 +0600

    home page layout complete

--- src/app/(home)/CreateTeam.tsx
+++ src/app/(home)/CreateTeam.tsx
@@ -1,12 +1,10 @@
 "use client"
-
 import Modal from "@/components/Modal";
 import { useState } from "react";
 
 const CreateTeam = () => {
-
     const [isOpen, setIsOpen] = useState(false);
-
+     
     const openModal = () => {
         setIsOpen(true);
     };
@@ -24,7 +22,7 @@ const CreateTeam = () => {
             <div>
                 <button className="bg-white text-indigo-500 border
                   border-indigo-500 py-3 px-6 rounded hover:bg-indigo-500 hover:text-white transition duration-300 text-xl font-bold" onClick={openModal} >+ Create a Team</button>
-            </div>
+            </div> 
             {isOpen && <Modal></Modal>}
         </div>
     );
